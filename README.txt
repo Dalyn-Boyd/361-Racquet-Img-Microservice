25689c813dba2d3682cba4bd9fd096a6bc7ceb8d
Dalyn Boyd <dalyn.boyd@gmail.com>
2022-05-12 16:54:03 -0500

    refactored to use urls from internet instead of local files

--- README.txt
+++ README.txt
@@ -1,3 +1,8 @@
+# Course: CS361 - Software Engineering 1
+# Author: Jana Dalyn Boyd
+# Assignment: Microservice
+# Description: Writes the URL of a random image to a text file.
+
 Files Included (place in the same directory as your main project)
 
 micro.py
@@ -11,21 +16,7 @@ instructions.txt
 
 img_url.txt
     'micro.py' will write the url of the image to be displayed to this file
-    your main project file should read the url
+    your main project file should read this url
 
-Example of code that needs to be included in your main project.
-
-    # ask microservice for url for display image
-    instruct_file = open("instructions.txt", "w")
-    instruct_file.write("run")
-    instruct_file.close()
-    
-    # read url from microservice
-    img_url = open("img_url.txt", "r")
-    display_image = img_url.readline()
-    img_url.close()
-    
-    # clear instruction file
-    instruct_file = open("instructions.txt", "w")
-    instruct_file.truncate()
-    instruct_file.close()
+racquet_image.txt
+    list of racquet urls
--- micro.py
+++ micro.py
@@ -1,3 +1,8 @@
+# Course: CS361 - Software Engineering 1
+# Author: Jana Dalyn Boyd
+# Assignment: Microservice
+# Description: Writes the URL of a random image to a text file.
+
 import random
 
 # repeat while site is live
@@ -7,14 +12,28 @@ while True:
     instruct_file = open("instructions.txt", "r")
     if instruct_file.readline() == 'run':
         
+        # hold urls
+        all_images = {}
+
+        # count the urls 
+        image_file = open("racquet_image.txt", "r")
+        url_count = len(image_file.readlines()) # Referenced: PYnative article 'Python Count Number of Lines in a File'
+
+        # add urls to dictionary
+        with open("racquet_image.txt", "r") as image_file:
+            for line in image_file:
+                curr_line = line.strip()
+                all_images[url_count] = curr_line
+                url_count -= 1
+
+        image_file.close()
+
         # generate random number
         random.seed(a=None, version=2)
-        ran_num = random.randrange(10)
- 
-        # URL to where images are
-        file_path = 'C:\Oregon State\CS 361\Main Project\static\images{}.jpg'.format(ran_num)
+        ran_num = random.randint(1, len(all_images))
+        print(all_images[ran_num])
 
         # open file & write URL
         img_url = open("img_url.txt", "w")
-        img_url.write(file_path)
+        img_url.write(all_images[ran_num])
         img_url.close()
