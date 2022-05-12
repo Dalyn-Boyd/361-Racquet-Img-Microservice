6e54960535ac0749666d2c9d7a42203c738004c2
Dalyn Boyd <dalyn.boyd@gmail.com>
2022-05-12 15:56:38 -0500

    rewrote micro.py to read 'run' command from txt file, added README'

--- micro.py
+++ micro.py
@@ -1,22 +1,20 @@
 import random
 
-def get_image():
-    """Generates the url of a random raquet image and writes it to a text file."""
-
-    # Generate random number
-    random.seed(a=None, version=2)
-    ran_num = random.randrange(10)
-
-    # TODO update depending on number of images      
-    if ran_num >= 4:
-        ran_num = ran_num % 4
-        
-    # update to URL where images are
-    file_path = 'C:\Oregon State\CS 361\Main Project\static\images{}.jpg'.format(ran_num)
+# repeat while site is live
+while True:
     
-    # open file 
-    img_url = open("img_url.txt", "w")
-    # write to file
-    img_url.write(file_path)
-    # close file 
-    img_url.close()
+    # look for run command
+    instruct_file = open("instructions.txt", "r")
+    if instruct_file.readline() == 'run':
+        
+        # generate random number
+        random.seed(a=None, version=2)
+        ran_num = random.randrange(10)
+ 
+        # URL to where images are
+        file_path = 'C:\Oregon State\CS 361\Main Project\static\images{}.jpg'.format(ran_num)
+
+        # open file & write URL
+        img_url = open("img_url.txt", "w")
+        img_url.write(file_path)
+        img_url.close()
