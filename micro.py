cbc92deeee36c8236ca0919d2419f56c03bd74b5
Dalyn Boyd <dalyn.boyd@gmail.com>
2022-05-22 12:45:58 -0500

    altered microservice to provide specific path url

--- micro.py
+++ micro.py
@@ -11,28 +11,16 @@ while True:
     # look for run command
     instruct_file = open("instructions.txt", "r")
     if instruct_file.readline() == 'run':
-        
-        # hold urls
-        all_images = {}
-
-        # count the urls 
-        image_file = open("racquet_image.txt", "r")
-        url_count = len(image_file.readlines()) # Referenced: PYnative article 'Python Count Number of Lines in a File'
-
-        # add urls to dictionary
-        with open("racquet_image.txt", "r") as image_file:
-            for line in image_file:
-                curr_line = line.strip()
-                all_images[url_count] = curr_line
-                url_count -= 1
-
-        image_file.close()
+        instruct_file.close()
+        instruct_file = open("instructions.txt", "w")
+        instruct_file.write("")
+        instruct_file.close()
 
         # generate random number
         random.seed(a=None, version=2)
-        ran_num = random.randint(1, len(all_images))
+        ran_num = random.randint(1, 7)
 
         # open file & write URL
         img_url = open("img_url.txt", "w")
-        img_url.write(all_images[ran_num])
+        img_url.write("microservice/" + str(ran_num) + ".jpg")
         img_url.close()
